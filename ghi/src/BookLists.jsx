5eb32d60f9db3ca866233a6aac2c582f839efc63
Killian Kim <killianjyk@gmail.com>
2023-06-06 12:50:38 -0500

    Modified loading.. to an animation

--- ghi/src/BookLists.jsx
+++ ghi/src/BookLists.jsx
@@ -37,7 +37,18 @@ const BookLists = ({ name, username }) => {
     previousLoad ||
     nextLoad
   )
-    return <div className="txt">Loading...</div>;
+    return (
+      <div className="mt-4 flex justify-center items-center">
+        <div className="book">
+          <div className="book__pg-shadow"></div>
+          <div className="book__pg"></div>
+          <div className="book__pg book__pg--2"></div>
+          <div className="book__pg book__pg--3"></div>
+          <div className="book__pg book__pg--4"></div>
+          <div className="book__pg book__pg--5"></div>
+        </div>
+      </div>
+    );
 
   let num = 1;
 
--- ghi/src/UserLists.jsx
+++ ghi/src/UserLists.jsx
@@ -16,7 +16,18 @@ const UserLists = ({ name }) => {
   }, []);
 
   if (searchLoad) {
-    return <div className="flex justify-center items-center">Loading...</div>;
+    return (
+      <div className="mt-4 flex justify-center items-center">
+        <div className="book">
+          <div className="book__pg-shadow"></div>
+          <div className="book__pg"></div>
+          <div className="book__pg book__pg--2"></div>
+          <div className="book__pg book__pg--3"></div>
+          <div className="book__pg book__pg--4"></div>
+          <div className="book__pg book__pg--5"></div>
+        </div>
+      </div>
+    );
   }
 
   if (!searchUsers || searchUsers.length === 0) {
