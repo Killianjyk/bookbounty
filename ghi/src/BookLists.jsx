342cc855a23bb0ef517fdc0183ae576337b004da
rew <rewgwolf@gmail.com>
2023-06-06 12:18:42 -0500

    styling dis users, search button, list titles

--- ghi/src/BookLists.jsx
+++ ghi/src/BookLists.jsx
@@ -1,36 +1,65 @@
 import { useSelector } from "react-redux";
 import {
-    useGetBookSearchQuery,
-    useGetTopFavoriteBooksQuery,
+  useGetBookSearchQuery,
+  useGetTopFavoriteBooksQuery,
 } from "./app/booksApiSlice";
 import {
-    useGetFavoriteBooksQuery,
-    useGetPreviousBooksQuery,
-    useGetNextBooksQuery,
-} from "./app/listApiSlice"
+  useGetFavoriteBooksQuery,
+  useGetPreviousBooksQuery,
+  useGetNextBooksQuery,
+} from "./app/listApiSlice";
 import BookCard from "./BookCard";
 
-
 const BookLists = ({ name, username }) => {
-    const searchField = useSelector((state) => state.searchField.value);
-    const { data: searchBooks, isLoading: searchLoad } = useGetBookSearchQuery(searchField, {skip: name!=="Search Books" || searchField==="" });
-    const { data: topFavoriteBooks, isLoading: topFavoritesLoad } = useGetTopFavoriteBooksQuery({skip: name!=="Most Liked"});
-    const { data: favoriteBooks, isLoading: favoritesLoad } = useGetFavoriteBooksQuery(username, {skip: name!=="Favorites" || username===undefined});
-    const { data: previousBooks, isLoading: previousLoad } = useGetPreviousBooksQuery(username, {skip: name!=="Previously Read" || username===undefined});
-    const { data: nextBooks, isLoading: nextLoad } = useGetNextBooksQuery(username, {skip: name!=="Read Next" || username===undefined});
-    if (searchLoad || topFavoritesLoad || favoritesLoad || previousLoad || nextLoad) return <div className="txt">Loading...</div>;
+  const searchField = useSelector((state) => state.searchField.value);
+  const { data: searchBooks, isLoading: searchLoad } = useGetBookSearchQuery(
+    searchField,
+    { skip: name !== "Search Books" || searchField === "" }
+  );
+  const { data: topFavoriteBooks, isLoading: topFavoritesLoad } =
+    useGetTopFavoriteBooksQuery({ skip: name !== "Most Liked" });
+  const { data: favoriteBooks, isLoading: favoritesLoad } =
+    useGetFavoriteBooksQuery(username, {
+      skip: name !== "Favorites" || username === undefined,
+    });
+  const { data: previousBooks, isLoading: previousLoad } =
+    useGetPreviousBooksQuery(username, {
+      skip: name !== "Previously Read" || username === undefined,
+    });
+  const { data: nextBooks, isLoading: nextLoad } = useGetNextBooksQuery(
+    username,
+    { skip: name !== "Read Next" || username === undefined }
+  );
+  if (
+    searchLoad ||
+    topFavoritesLoad ||
+    favoritesLoad ||
+    previousLoad ||
+    nextLoad
+  )
+    return <div className="txt">Loading...</div>;
 
-    let num = 1;
+  let num = 1;
 
-    return (<>
-        <h1 className="txt text-3xl text-center mt-4">{name}</h1>
-        {name==="Search Books" && searchBooks?.map((book) => <BookCard book={book} />)}
+  let listTitle = name;
+  if (username) {
+    listTitle = `${username}- ${name}`;
+  }
 
+  return (
+    <>
+      <h1 className="txt text-3xl text-center mt-4">{listTitle}</h1>
+      {name === "Search Books" &&
+        searchBooks?.map((book) => <BookCard book={book} />)}
 
-        {name==="Favorites" && favoriteBooks?.map((book) => <BookCard book={book} />)}
-        {name==="Previously Read" && previousBooks?.map((book) => <BookCard book={book} />)}
-        {name==="Read Next" && nextBooks?.map((book) => <BookCard book={book} />)}
-    </>);
-}
+      {name === "Favorites" &&
+        favoriteBooks?.map((book) => <BookCard book={book} />)}
+      {name === "Previously Read" &&
+        previousBooks?.map((book) => <BookCard book={book} />)}
+      {name === "Read Next" &&
+        nextBooks?.map((book) => <BookCard book={book} />)}
+    </>
+  );
+};
 
 export default BookLists;
--- ghi/src/UserLists.jsx
+++ ghi/src/UserLists.jsx
@@ -4,24 +4,42 @@ import { useGetUserSearchQuery } from "./app/authApiSlice";
 import { useEffect } from "react";
 import UserCard from "./UserCard";
 
-
 const UserLists = ({ name }) => {
-    const dispatch = useDispatch();
-    const searchField = useSelector((state) => state.searchField.value);
-    const { data: searchUsers, isLoading: searchLoad } = useGetUserSearchQuery(searchField, { skip: name!=="User Search" });
-    useEffect(() => {
-        dispatch(search(searchField));
-    }, []);
-    if (searchLoad) {
-        return <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center',}}>Loading...</div>;
-    }
-    if (!searchUsers || searchUsers.length === 0) {
-        return <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center',}}>No matching result found</div>;
-    }
-    return (<>
-        <h1 className="txt">{name}</h1>
-        {name==="User Search" && searchUsers?.map((user) => <UserCard user={user} />)}
-    </>);
-}
+  const dispatch = useDispatch();
+  const searchField = useSelector((state) => state.searchField.value);
+  const { data: searchUsers, isLoading: searchLoad } = useGetUserSearchQuery(
+    searchField,
+    { skip: name !== "User Search" }
+  );
+  useEffect(() => {
+    dispatch(search(searchField));
+  }, []);
+
+  if (searchLoad) {
+    return <div className="flex justify-center items-center">Loading...</div>;
+  }
+
+  if (!searchUsers || searchUsers.length === 0) {
+    return (
+      <div className="flex justify-center items-center">
+        No matching result found
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <h1 className="txt">{name}</h1>
+      <div className="grid grid-cols-3 gap-4">
+        {name === "User Search" &&
+          searchUsers.map((user) => (
+            <div className="p-4">
+              <UserCard user={user} />
+            </div>
+          ))}
+      </div>
+    </>
+  );
+};
 
 export default UserLists;
